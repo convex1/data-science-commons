4305bea9a8d3cab7cc21ebae124631c3a08367ad
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-05 21:32:15 -0700

    adding SQL functions

--- SQL/window_functions.sql
+++ SQL/window_functions.sql
@@ -5,7 +5,7 @@
     123      | 0.98
 
 --to rank (with tie) records within a group in decreasing order of their score
-select group_id, score, dense_rank() parition by (group_id order by score desc);
+select group_id, score, dense_rank() over(partition by group_id order by score desc);
 
 --to rank (with no tie) records within a group in decreasing order of their score
-select group_id, score, row_number() parition by (group_id order by score desc);
+select group_id, score, row_number() over(partition by group_id order by score desc);
