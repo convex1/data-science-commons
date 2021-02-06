388600a39ece789d6330fd9b6567350de1db6d80
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-05 21:50:13 -0700

    adding spaces

--- SQL/window_functions.sql
+++ SQL/window_functions.sql
@@ -5,6 +5,11 @@
 --- 123      | 0.85
 --- 123      | 0.98
 
+
+
+
+
+
 --to rank (with tie) records within a partition (group in this case) in decreasing order of their score
 
 select group_id, score, dense_rank() over(partition by group_id order by score desc) rank from group_table;
@@ -14,6 +19,11 @@ select group_id, score, dense_rank() over(partition by group_id order by score d
     123      | 0.98  | 1
     123      | 0.85  | 2
     123      | 0.76  | 2
+
+
+
+
+
 
 --to rank (with no tie) records within a partition (group in this case) in decreasing order of their score
 
@@ -26,6 +36,11 @@ select group_id, score, row_number() over(partition by group_id order by score d
     123      | 0.76  | 3
 
 
+
+
+
+
+
 --to get immediate previous value of row/records within a partition (group in this case)
 
 select group_id, score, lag(score,1) over(partition by group_id) previous_score;
@@ -35,6 +50,11 @@ select group_id, score, lag(score,1) over(partition by group_id) previous_score;
     123      | 0.85  | null
     123      | 0.85  | 0.85
     123      | 0.98  | 0.85
+
+
+
+
+
 
 --to get immediate next value of row/records within a partition (group in this case)
 
@@ -46,6 +66,11 @@ select group_id, score, lead(score,1) over(partition by group_id) next_score;
     123      | 0.85  | 0.98
     123      | 0.98  | null
 
+
+
+
+
+
 --to calculate running average within a partition (group in this case)
 
 select group_id, score, avg(score) over(partition by group_id) running_average;
@@ -55,6 +80,11 @@ select group_id, score, avg(score) over(partition by group_id) running_average;
     123      | 0.85  | 0.85
     123      | 0.85  | 0.85
     123      | 0.98  | 0.893
+
+
+
+
+
 
 --to calculate running sum within a partition (group in this case)
 
