926c3c90d3518520b9079664e9ce04b3da79b339
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-05 21:48:59 -0700

    adding window functions

--- SQL/window_functions.sql
+++ SQL/window_functions.sql
@@ -7,7 +7,7 @@
 
 --to rank (with tie) records within a partition (group in this case) in decreasing order of their score
 
-select group_id, score, dense_rank() over(partition by group_id order by score desc) as rank from group_table;
+select group_id, score, dense_rank() over(partition by group_id order by score desc) rank from group_table;
 
 ---output
 --- group_id | score | rank
@@ -17,7 +17,7 @@ select group_id, score, dense_rank() over(partition by group_id order by score d
 
 --to rank (with no tie) records within a partition (group in this case) in decreasing order of their score
 
-select group_id, score, row_number() over(partition by group_id order by score desc);
+select group_id, score, row_number() over(partition by group_id order by score desc) rank;
 
 ---output
 --- group_id | score | rank
@@ -28,11 +28,42 @@ select group_id, score, row_number() over(partition by group_id order by score d
 
 --to get immediate previous value of row/records within a partition (group in this case)
 
-select group_id, score, lag(score,1) over(partition by group_id);
+select group_id, score, lag(score,1) over(partition by group_id) previous_score;
 
 ---output
 --- group_id | score | rank
     123      | 0.85  | null
     123      | 0.85  | 0.85
     123      | 0.98  | 0.85
+
+--to get immediate next value of row/records within a partition (group in this case)
+
+select group_id, score, lead(score,1) over(partition by group_id) next_score;
+
+---output
+--- group_id | score | rank
+    123      | 0.85  | 0.85
+    123      | 0.85  | 0.98
+    123      | 0.98  | null
+
+--to calculate running average within a partition (group in this case)
+
+select group_id, score, avg(score) over(partition by group_id) running_average;
+
+---output
+--- group_id | score | rank
+    123      | 0.85  | 0.85
+    123      | 0.85  | 0.85
+    123      | 0.98  | 0.893
+
+--to calculate running sum within a partition (group in this case)
+
+select group_id, score, sum(score) over(partition by group_id) running_sum;
+
+---output
+--- group_id | score | rank
+    123      | 0.85  | 0.85
+    123      | 0.85  | 1.70
+    123      | 0.98  | 2.68
+
 
