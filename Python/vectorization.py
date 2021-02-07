018d8005971e1ae40f0ffe8bb1aa203614b1168f
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-07 00:30:47 -0700

    adding vectorizatoin" git push git add Python/vectorization.p; ; "

--- Python/vectorization.py
+++ Python/vectorization.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
 
-data = {"name": ["a", "b"], "city": ["NY", "SEA"]}
-dataframe = pd.DataFrame(data=data)
+#create dummy dataframe about dragon ball z characters earth location and other information
+
+data = {"name": ["goku", "gohan"], "city": ["NY", "SEA"]}
+dragon_ball_on_earth = pd.DataFrame(data=data)
+
+#create new series (column) by using vectorization
+dragon_ball_on_earth['is_goku'] = np.where(dragon_ball_on_earth['name'] == "goku", 1, 0)
