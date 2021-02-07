d830be0334ef619518d493905fdffbbb0ca93973
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-07 00:38:33 -0700

    adding more vectorization" git push exit

--- Python/vectorization.py
+++ Python/vectorization.py
@@ -17,6 +17,21 @@ dragon_ball_on_earth = pd.DataFrame(data=data)
 
 """
 create new series (column) by using vectorization
+there is one single condition and one single outcome except the default
 """
 
 dragon_ball_on_earth['is_goku'] = np.where(dragon_ball_on_earth['name'] == "goku", 1, 0)
+
+
+"""
+How to assign the series column based on multiple conditions?
+
+Use np.select() instead of np.where()
+np.select() can take multiple conditions and multiple outcomes
+
+"""
+
+conditions =[[dragon_ball_on_earth['name'] == "goku"], [dragon_ball_on_earth['name'] == "gohan"]]
+outcomes = [1,2]
+
+dragon_ball_on_earth['coded_name'] = np.select(conditions, 1, 0)
