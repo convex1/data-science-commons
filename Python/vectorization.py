59be80d5dd248ebddbfd1d7720569b7e87d7ae5e
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-07 00:33:49 -0700

    adding doc strings

--- Python/vectorization.py
+++ Python/vectorization.py
@@ -1,10 +1,22 @@
 import pandas as pd
 import numpy as np
 
-#create dummy dataframe about dragon ball z characters earth location and other information
+"""
+~~ABOUT~~
+Use vectorization instead of using for loops to assign new values. Try to do it whenever possible.
+It will be possible in most cases except a few minor comlicated cases where for loop might be required.None
+"""
+
+
+"""
+create dummy dataframe about dragon ball z characters earth location and other information
+"""
 
 data = {"name": ["goku", "gohan"], "city": ["NY", "SEA"]}
 dragon_ball_on_earth = pd.DataFrame(data=data)
 
-#create new series (column) by using vectorization
+"""
+create new series (column) by using vectorization
+"""
+
 dragon_ball_on_earth['is_goku'] = np.where(dragon_ball_on_earth['name'] == "goku", 1, 0)
