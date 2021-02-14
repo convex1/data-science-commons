d8f73f5ce6cd404552af512b8ab9a8f3180cb07a
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-13 17:00:13 -0700

    aggregating-concatenating string values

--- SQL/aggregate_functions.sql
+++ SQL/aggregate_functions.sql
@@ -1,9 +1,9 @@
 ---input
 ---group_table
---- group_id | score |
---- 123      | 0.85
---- 123      | 0.85
---- 123      | 0.98
+--- group_id | score | char_value
+--- 123      | 0.85  |  A
+--- 123      | 0.85  |  B
+--- 123      | 0.98  |  C
 
 
 
@@ -14,5 +14,15 @@ select group_id, sum(score) as sum_score, avg(score) avg_score, count(score) cou
 group by group_id;
 
 ---output
---- group_id | sum_score | avg_score | count_score
-    123      | 2.68 | 0.893 | 3
+group_id | sum_score | avg_score | count_score
+123      | 2.68 | 0.893 | 3
+
+
+
+
+select group_id, string_agg(char_value, ',') as char_values from group_table
+group by group_id;
+
+---output
+--- group_id | char_values
+    123      | A,B,C
