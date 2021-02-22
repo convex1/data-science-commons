a7c1fdf6e58d692cea24e080a07e0d4617d1dd05
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-02-21 19:15:28 -0700

    adding list functions for python

---
+++ Python/list_operations.py
@@ -0,0 +1,39 @@
+#dummy list
+
+items_list = [1,2,3,4,5]
+
+
+#sort the list
+sorted(items_list)
+sorted(items_list, reverse=True)
+
+
+#insert element to the list
+#the code below inserts value 1 to index 0
+items_list.insert(0,1)
+
+#remove element from list
+items_list.pop()
+#below removes element at 1st index
+items_list.pop(1)
+
+
+#list comprehensiions
+even_numbers_only = [x for x in items_list if x % 2 === 0]
+square_each_element = [x ** 2 for x in items_list]
+
+
+#list of strings
+string_list = ["abc", "def", "xyz"]
+
+#list comprehensiions
+strings_length_greater_than_3 = [x for x in items_list if len(x) >= 3]
+strings_starting with_a  = [x for x in items_list if x[0] == "a"]
+
+
+#join all strings in a list as single string - concatenation
+''.join(string_list)
+
+
+
+
