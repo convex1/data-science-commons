d7ae9e51366cded7c68389adb1be2d32a552a171
SRIJAN  BHUSHAN <srijanbhushan@SRIJANs-MacBook-Pro.local>
2021-03-21 23:02:39 -0600

    adding dictionary operations

---
+++ Python/dictionary_operations.py
@@ -0,0 +1,12 @@
+#dummy list
+
+sample_dictionary = {'aarav': 1, "albert": 2}
+
+#print keys and values of the dictionary
+print sample_dictionary.keys()
+print sample_dictionary.values()
+
+#add dictionary element
+sample_dictionary["srijan"] = 21
+#update dictionary element
+sample_dictionary["albert"] = 4
